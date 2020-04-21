fa057c27e738b056ea64a1cbc4953c6e9f8d05a4
Radu Sibechi <radu.sibechi@gmail.com>
2020-04-21 22:07:14 +0200

    now users can sign up without access token

--- api/permissions.py
+++ api/permissions.py
@@ -1,15 +1,32 @@
-from rest_framework import permissions
+from rest_framework.permissions import BasePermission
 
-class IsOwnerOrReadOnly(permissions.BasePermission):
+class IsOwnerOrReadOnly(BasePermission):
     """
     Custom permission to only allow owners of an object to edit it.
     """
+    SAFE_METHODS = ['GET', 'HEAD', 'OPTIONS']
 
     def has_object_permission(self, request, view, obj):
         # Read permissions are allowed to any request,
         # so we'll always allow GET, HEAD or OPTIONS requests.
-        if request.method in permissions.SAFE_METHODS:
+        if request.method in self.SAFE_METHODS:
             return True
 
         # Write permissions are only allowed to the owner of the snippet.
         return obj.owner == request.user
+
+
+class IsCreatingHasAccessOrNoAccess(BasePermission):
+    """
+    Either the user is trying to sign up, or no permissions
+    """
+
+    SAFE_METHODS = ['POST']
+
+    def has_permission(self, request, view):
+        if (request.method in self.SAFE_METHODS or
+            (request.user and request.user.is_authenticated)
+            ):
+            return True
+
+        return False
--- api/views.py
+++ api/views.py
@@ -1,17 +1,19 @@
-from rest_framework import generics, permissions
+from rest_framework import generics
+from rest_framework import permissions as base_permissions
 
+from api.permissions import IsCreatingHasAccessOrNoAccess
 from api.models import User, Jog, AuthRole
 from api.serializers import UserSerializer, JogSerializer, AuthRoleSerializer
 
 class UserList(generics.ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [IsCreatingHasAccessOrNoAccess]
 
 class JogList(generics.ListCreateAPIView):
     queryset = Jog.objects.all()
     serializer_class = JogSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [base_permissions.IsAuthenticatedOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(user_id=self.request.user)
@@ -19,7 +21,7 @@ class JogList(generics.ListCreateAPIView):
 class AuthRoleList(generics.ListCreateAPIView):
     queryset = AuthRole.objects.all()
     serializer_class = AuthRoleSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [base_permissions.IsAuthenticatedOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(user_id=self.request.user)
@@ -27,17 +29,17 @@ class AuthRoleList(generics.ListCreateAPIView):
 class UserDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [base_permissions.IsAuthenticatedOrReadOnly]
 
 class JogDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Jog.objects.all()
     serializer_class = JogSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [base_permissions.IsAuthenticatedOrReadOnly]
 
 class AuthRoleDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = AuthRole.objects.all()
     serializer_class = AuthRoleSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [base_permissions.IsAuthenticatedOrReadOnly]
 
 
 
